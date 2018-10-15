2b1179c12a34ad1707c6e784cec652edb90fa25a
Kazuki Tobita <kazukiti201@gmail.com>
2018-10-15 15:06:52 +0900

    Modify design

--- resources/views/items.blade.php
+++ resources/views/items.blade.php
@@ -2,14 +2,16 @@
 
 @section('content')
     <div class="container">
-        @foreach($items as $item)
-            <div class="">
-                <img src="{{ $item->img }}" alt="">
-                <p>{{ $item->name }}</p>
-                <p>{{ $item->description }}</p>
-                <a href="{{ '/items/' . $item->id }}" class="btn btn-primary">Detail</a>
-                <button type="button" class="btn btn-primary">Add to Cart</button>
-            </div>
-        @endforeach
+        <div class="row">
+            @foreach($items as $item)
+                <div class="m-2">
+                    <img src="{{ $item->img }}" alt="">
+                    <p>{{ $item->name }}</p>
+                    <p>{{ $item->description }}</p>
+                    <a href="{{ '/items/' . $item->id }}" class="btn btn-primary">Detail</a>
+                    <button type="button" class="btn btn-primary">Add to Cart</button>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
