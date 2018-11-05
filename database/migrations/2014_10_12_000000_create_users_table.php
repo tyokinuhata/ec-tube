e0df17b0bbec018e385714386fc6da866855b70f
Kazuki Tobita <kazukiti201@gmail.com>
2018-11-05 15:49:49 +0900

    Modify migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,10 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('postal_number');
+            $table->text('address');
+            $table->json('carts');
             $table->rememberToken();
             $table->timestamps();
         });
