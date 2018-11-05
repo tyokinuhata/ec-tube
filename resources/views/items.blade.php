283f3aad20cf86f1aa474a528f91c9ae01260aad
Kazuki Tobita <kazukiti201@gmail.com>
2018-11-05 15:41:08 +0900

    Modify items.blade.php design

--- resources/views/items.blade.php
+++ resources/views/items.blade.php
@@ -6,7 +6,7 @@
             @foreach($items as $item)
                 <div class="m-2">
                     <img src="{{ $item->img }}" alt="">
-                    <p>{{ $item->name }}</p>
+                    <p class="h4 mt-2">{{ $item->name }}</p>
                     <p>{{ $item->description }}</p>
                     <a href="{{ '/items/' . $item->id }}" class="btn btn-primary">Detail</a>
                     <button type="button" class="btn btn-primary">Add to Cart</button>
