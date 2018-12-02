27ac5e42af3b7b1f516f886eab172411a8634c65
Kazuki Tobita <kazukiti201@gmail.com>
2018-12-03 06:20:17 +0900

    Modify sushi

--- resources/views/foods/index.blade.php
+++ resources/views/foods/index.blade.php
@@ -1,29 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            @foreach($foods as $food)
-                <div class="m-2">
-                    <img src="{{ $food->img }}" alt="" width="100">
-                    <p class="h4 mt-2">{{ $food->name }}</p>
-                    <p>{{ $food->description }}</p>
-                    <form method="POST" action="{{ url('foods') }}" class="d-inline">
-                        @csrf
-                        <input type="hidden" value="{{ $food->id }}" name="item">
-                        <input type="number" value="1" required class="form-control d-inline">
-                        <button type="submit" class="btn btn-primary d-inline">Add to Cart</button>
-                    </form>
-                </div>
-            @endforeach
-        </div>
-    </div>
     <div class="lanes-wrapper">
         <div class="lanes">
             @foreach($foods as $food)
                 <div class="lane">
                     <div class="dish">
-                        <img src="{{ $food->img }}" alt="" class="sushi">
+                        <img src="{{ $food->img }}" alt="" class="sushi" data-toggle="modal" data-target="#modal{{ $food->id }}">
+                    </div>
+                </div>
+
+                <div class="modal fade" id="modal{{ $food->id }}" tabindex="-1" role="dialog" aria-labelledby="modalLabel{{ $food->id }}" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">食べますか？</h5>
+                            </div>
+                            <div class="modal-body">
+                                <p class="h4 mt-2">{{ $food->name }}</p>
+                                <p>{{ $food->description }}</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">食べない</button>
+                                <form method="POST" action="{{ url('foods') }}">
+                                    @csrf
+                                    <input type="number" class="form-control d-inline col-md-3" name="number" value="0">
+                                    <input type="hidden" name="id" value="{{ $food->id }}">
+                                    <button type="submit" class="btn btn-primary">食べる！</button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             @endforeach
